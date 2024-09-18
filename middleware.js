392ac020a6e967d6cc17010eb368f84a795d6431
laith <laithalwani@gmail.com>
2024-09-18 12:02:01 -0400

    fixes issue

--- middleware.js
+++ middleware.js
@@ -7,6 +7,7 @@ const isPublicRoute = createRouteMatcher([
   "/thank-you",
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/api/*"
 ]);
 
 export default clerkMiddleware((auth, request) => {
@@ -19,6 +20,7 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
 };
